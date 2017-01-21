5f547d50e398d30c412f41f77addf3ccc0c475a6
Monika Krauze <m.krauze@visuality.pl>
2017-01-21 11:31:17 +0100

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('watch', function() {
   gulp.watch( './app/assets/scss/**/*.scss', ['scss'] );
 });
 
 gulp.task('sass', function(){
-    return gulp.src('./app/assets/scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-        errLogToConsole: true,
-        outputStyle: 'expanded', 
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./app/assets/css/'))
+  return gulp.src('./app/assets/scss/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+  errLogToConsole: true,
+  outputStyle: 'expanded', 
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./app/assets/css/'))
 })
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./app/assets/scss/**/*.scss', ['sass'])
+  gulp.watch('./app/assets/scss/**/*.scss', ['sass'])
 });
