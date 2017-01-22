e5c55f044a482acc0c75d0ebce6c72c9fe0b8a70
Monika Krauze <m.krauze@visuality.pl>
2017-01-22 14:30:43 +0100

    Create modals (styling not finished)

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+  selectTshirt();
+
+  $("#pizza-master-button").on("click", function() {
+    UIkit.modal("#pizza-master-modal", {center:true}).show();
+  })
+
+  $("#pizza-master-show").on("click", function() {
+    UIkit.modal("#pizza-master-design", {center:true}).show();
+  })
+  
+});
+
+function selectTshirt () {
+  $(".t-shirt__single").hover(function() {
+    var tshirt = $(this).find(".t-shirts__piece");
+    var button = $(this).find(".t-shirts__button");
+    var src = (tshirt.attr('src') === './assets/img/tshirt.png')
+      ? './assets/img/tshirt_active.png'
+      : './assets/img/tshirt.png';
+      
+    tshirt.attr('src', src);
+    button.toggleClass("active");
+  });
+}
