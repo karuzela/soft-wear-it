6cd6abe1f67f91a7c242a40573eff4581704e332
Monika Krauze <m.krauze@visuality.pl>
2017-01-23 20:01:04 +0100

    Add missing modals for the rest of the t-shirts

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,20 +3,21 @@ $(document).ready(function() {
 
   selectTshirt();
 
-  $("#pizza-master-button").on("click", function() {
-    UIkit.modal("#pizza-master-modal", {center:true}).show();
+  $('[id$="-button"]').on("click", function() {
+    var modalChooseTshirt = $(this).closest(".t-shirts__piece-container").find('.modal-choose-t-shirt__container');
+    UIkit.modal(modalChooseTshirt, {center:true}).show();
   })
 
-  $("#pizza-master-show").on("click", function() {
-    UIkit.modal("#pizza-master-design", {center:true}).show();
-  })
-
-  $(".button-close-zoom").on("click", function() {
-    UIkit.modal("#pizza-master-design").hide();
-  })
+  $('[id$="-show"]').on("click", function() {
+    var modalZoom = $(this).closest(".t-shirts__piece-container").find('.modal-zoom__container');
+    UIkit.modal(modalZoom, {center:true}).show();
+      $(".button-close-zoom").on("click", function() {
+        UIkit.modal(modalZoom).hide();
+      })
+    })
 });
 
-function selectTshirt () {
+function selectTshirt() {
   $(".t-shirt__single").hover(function() {
     var tshirt = $(this).find(".t-shirts__piece");
     var button = $(this).find(".t-shirts__button");
@@ -28,3 +29,5 @@ function selectTshirt () {
     button.toggleClass("active");
   });
 }
+
+
