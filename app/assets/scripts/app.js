57afde5a386cc3a27131a4bea6618fb091eab5ce
Monika Krauze <m.krauze@visuality.pl>
2017-01-24 11:36:35 +0100

    Fix fonts and mobile display, add close button to choose  t-shirt modal in mobile version

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -6,15 +6,18 @@ $(document).ready(function() {
   $('[id$="-button"]').on("click", function() {
     var modalChooseTshirt = $(this).closest(".t-shirts__piece-container").find('.modal-choose-t-shirt__container');
     UIkit.modal(modalChooseTshirt, {center:true}).show();
+    $(".button-close-tshirt").on("click", function() {
+      UIkit.modal(modalChooseTshirt).hide();
+    })
   })
 
   $('[id$="-show"]').on("click", function() {
     var modalZoom = $(this).closest(".t-shirts__piece-container").find('.modal-zoom__container');
     UIkit.modal(modalZoom, {center:true}).show();
-      $(".button-close-zoom").on("click", function() {
-        UIkit.modal(modalZoom).hide();
-      })
+    $(".button-close-zoom").on("click", function() {
+      UIkit.modal(modalZoom).hide();
     })
+  })
 });
 
 function selectTshirt() {
